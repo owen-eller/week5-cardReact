0e1b67f0d56b2cc616280df17fb63ca679d87698
Owen <oweneller@att.net>
2023-09-28 12:14:31 -0400

    week6-react

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ function App() {
 
 
     return (
-      <div className="App">
+    <html>
+      <meta charset="UTF-8"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <link rel="stylesheet" href="styles.css"></link>
+      <title>Card</title>
       
-        <h1>Card</h1>
-          <h2 class="card-title">HAX PSU</h2>
-          <img src="https://hax.camp/assets/haxBanner-01.png" alt="Card Image"></img>
-          <div class="description">Hax Camp 2022</div>
-          <a class="details-link" href="https://hax.psu.edu">Details</a>
+      <div class="wrapper">
+        <div class="container" data-card="1">
+          <div class="card">
+            <div class="card-content">
+              <h2 class="card-title">HAX PSU</h2>
+              <img src="https://hax.camp/assets/haxBanner-01.png" alt="Card Image"></img>
+              <div class="description-toggle">
+                <div class="description">Hax Camp 2022</div>
+                <div class="toggle-button">Toggle Description</div>
+              </div>
+              <a class="details-link" href="https://hax.psu.edu">Details</a>
+            </div>
+          </div>
+        </div>
       </div>
+      <button class="duplicateButton">Duplicate Card</button>
+<button class="toggleColorButton">Toggle Color</button>
+<button class="changeTitleButton">Change Title</button>
+<button class="deleteCardButton">Delete Last Card</button>
+    </html>
     
 
     );
